44760ad5640ac7b37060cdd8a89566f5a314e643
hcrj99 <hcrj99@hotmail.com>
2023-12-04 20:04:10 -0500

    upgrades

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import '../styles/work.css'
-import { CgCloseR } from "react-icons/cg";
+import { CgCloseR } from 'react-icons/cg';
 
 function Work({ text }){
   return (
-    <section className="work__container">
-      <div className="work__text">
+    <section className='work__container'>
+      <div className='work__text'>
         {text}
       </div>
-      <div className="work__icon">
-        <CgCloseR />
+      <div className='work__container-icons'>
+        <CgCloseR className='work__icon'/>
       </div>
     </section>
   );
