d67bc70b8e1d9dfe687d390ae6f704782f038b7e
hcrj99 <hcrj99@hotmail.com>
2023-12-04 19:57:53 -0500

    add react icon

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/work.css'
-
+import { CgCloseR } from "react-icons/cg";
 
 function Work({ text }){
   return (
@@ -9,7 +9,7 @@ function Work({ text }){
         {text}
       </div>
       <div className="work__icon">
-        Eliminar
+        <CgCloseR />
       </div>
     </section>
   );
