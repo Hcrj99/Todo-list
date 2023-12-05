f145a41e61bfcc66ae114f80c419359da6863fd9
hcrj99 <hcrj99@hotmail.com>
2023-12-04 19:48:16 -0500

    struct work component render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Work from './components/work';
 
 function App() {
   return (
     <div className='Todo__List'>
       <section className='Todo__list-container'>
         <h1>My ToDo List</h1>
-        
+        <Work text='learn react'/>
       </section>
     </div>
   );
