1c582f708d7be82ee69a5b885a4dba5be63435a3
hcrj99 <hcrj99@hotmail.com>
2023-12-06 19:35:08 -0500

    add global work initial struct + style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Work from './components/work';
-import Formularyworks from './components/formularyworks';
+import Globalwork from './components/globalwork';
 
 function App() {
   return (
     <div className='Todo__List'>
       <section className='Todo__list-container'>
         <h1>My ToDo List</h1>
-        {/* <Work text='learn react'/> */}
-        <Formularyworks/>
+        <Globalwork />
       </section>
     </div>
   );
---
+++ src/components/globalwork.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Formularyworks from "./formularyworks";
+import '../styles/globalwork.css'
+
+function Globalwork(){
+  return (
+    <>
+      <Formularyworks/>
+      <section className="works__list-container">
+        LISTA DE TAREAS
+      </section>
+    </>
+  );
+}
+
+export default Globalwork;
